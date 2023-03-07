5762f5ff10853df525e37194161174f027817479
BrandonSGC <125517255+BrandonSGC@users.noreply.github.com>
2023-03-07 16:37:23 -0600

    Se crea la clase Conexion hacia MySQL

---
+++ proyecto-bases-datos/Conexion.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_bases_datos
+{
+    class Conexion
+    {
+        public static MySqlConnection conexion()
+        {
+            string servidor = "localhost";
+            string bd = "proyectodb";
+            string usuario = "root";
+            string contrasena = "root";
+
+            string cadenaConexion = $"Database={bd};Data Source={servidor};User Id={usuario};Password={contrasena}";
+
+            try
+            {
+                MySqlConnection conexionBD = new MySqlConnection(cadenaConexion);
+
+                return conexionBD;
+            }
+            catch (MySqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
+            }
+        }
+    }
+}
