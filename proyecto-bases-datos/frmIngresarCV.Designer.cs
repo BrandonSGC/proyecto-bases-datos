466c8def4374fe53f69dfa2a6f992271ea5049f8
BrandonSGC <125517255+BrandonSGC@users.noreply.github.com>
2023-03-07 13:36:22 -0600

    Funcionalidad de Lectura de Archivo

--- proyecto-bases-datos/frmIngresarCV.Designer.cs
+++ proyecto-bases-datos/frmIngresarCV.Designer.cs
@@ -28,13 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnLeerArchivo = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(15, 23);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(1172, 582);
+            this.richTextBox1.TabIndex = 4;
+            this.richTextBox1.Text = "";
+            // 
+            // btnLeerArchivo
+            // 
+            this.btnLeerArchivo.Location = new System.Drawing.Point(539, 611);
+            this.btnLeerArchivo.Name = "btnLeerArchivo";
+            this.btnLeerArchivo.Size = new System.Drawing.Size(112, 40);
+            this.btnLeerArchivo.TabIndex = 3;
+            this.btnLeerArchivo.Text = "Leer Archivo";
+            this.btnLeerArchivo.UseVisualStyleBackColor = true;
+            this.btnLeerArchivo.Click += new System.EventHandler(this.btnLeerArchivo_Click);
             // 
             // frmIngresarCV
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1202, 675);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.btnLeerArchivo);
             this.Name = "frmIngresarCV";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Ingresar CV";
@@ -43,5 +66,8 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnLeerArchivo;
     }
 }
--- proyecto-bases-datos/frmIngresarCV.cs
+++ proyecto-bases-datos/frmIngresarCV.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
+using Microsoft.Office.Interop.Word;
 
 namespace proyecto_bases_datos
 {
@@ -15,6 +17,24 @@ namespace proyecto_bases_datos
         public frmIngresarCV()
         {
             InitializeComponent();
+        }
+
+        private void btnLeerArchivo_Click(object sender, EventArgs e)
+        {
+            // Creamos la instancia del FileDialog para leer el archivo.
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.ShowDialog();
+
+            // Asignamos la ruta a variable "filePath".
+            string filePath = fileDialog.FileName.ToString();
+
+
+            // Leer el archivo de Word.
+            Microsoft.Office.Interop.Word.Application app = new Microsoft.Office.Interop.Word.Application();
+            Document doc = app.Documents.Open(filePath);
+            string data = doc.Content.Text;
+            richTextBox1.Text = data;
+            app.Quit();
         }
     }
 }
