8dfc9db83f4383f780244b71fb5406d44f7eb473
BrandonSGC <125517255+BrandonSGC@users.noreply.github.com>
2023-03-09 15:37:03 -0600

    Se agrega Clase Puesto

---
+++ proyecto-bases-datos/Puesto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_bases_datos
+{
+    class Puesto
+    {
+        private string nombre;
+        private string nombre_empresa;
+        private float salario;
+
+        public Puesto(string nombre, string nombre_empresa, float salario)
+        {
+            this.nombre = nombre;
+            this.nombre_empresa = nombre_empresa;
+            this.salario = salario;
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public string Nombre_empresa
+        {
+            get { return nombre_empresa; }
+            set { nombre = value; }
+        }
+    }
+}
