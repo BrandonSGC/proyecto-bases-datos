c2ea314b85f9d69c4577748d6141310552acd725
BrandonGC <brangmz20@gmail.com>
2023-04-01 17:46:31 -0600

    Funcionalidad de lectura de nueva planilla

--- proyecto-bases-datos/frmIngresarCV.cs
+++ proyecto-bases-datos/frmIngresarCV.cs
@@ -52,28 +52,73 @@ namespace proyecto_bases_datos
                 string line = paragraph.Range.Text.Trim();
                 string[] datos = line.Split(':');
 
-                if (cont == 0)
+                // Validamos que haya texto, sino salta de linea.
+                if (line.Length > 0)
                 {
-                    candidato.Cedula = int.Parse(datos[1].ToString());
-                }
-                if (cont == 1)
-                {
-                    candidato.Nombre = datos[1].ToString();
-                }
-                if (cont == 2)
-                {
-                    candidato.Apellidos = datos[1].ToString();
-                }
-                if (cont == 3)
-                {
-                    candidato.Fecha_nacimiento = datos[1].ToString();
-                }
-                if (cont == 4)
-                {
-                    candidato.Fecha_aplicacion = datos[1].ToString();
+                    if (cont == 0)
+                    {
+                        candidato.Cedula = int.Parse(datos[1].ToString());
+                    }
+                    if (cont == 1)
+                    {
+                        candidato.Nombre = datos[1].ToString();
+                    }
+                    if (cont == 2)
+                    {
+                        candidato.Apellidos = datos[1].ToString();
+                    }
+                    if (cont == 3)
+                    {
+                        candidato.Fecha_nacimiento = datos[1].ToString();
+                    }
+                    if (cont == 4)
+                    {
+                        candidato.Fecha_aplicacion = datos[1].ToString();
+                    }
+                    if (cont == 5)
+                    {
+                        string puesto = datos[1].ToString();
+                    }
+                    // EMPIEZA LA EXPERIENCIA
+                    if (cont == 8)
+                    {
+                        string empresa = datos[1].ToString();
+                    }
+                    if (cont == 9)
+                    {
+                        string fechaInicio = datos[1].ToString();
+                    }
+                    if (cont == 10)
+                    {
+                        string fechaFinalizacion = datos[1].ToString();
+                    }
+                    if (cont == 11)
+                    {
+                        string descripcion = datos[1].ToString();
+                    }
+                    // EMPIEZA LA EDUCACION.
+                    if (cont == 14)
+                    {
+                        string centroEducativo = datos[1].ToString();
+                    }
+
+                    if (cont == 15)
+                    {
+                        string fechaInicioEdu= datos[1].ToString();
+                    }
+
+                    if (cont == 14)
+                    {
+                        string fechaFinalizacionEdu = datos[1].ToString();
+                    }
+
+                    if (cont == 14)
+                    {
+                        string descripcionEdu = datos[1].ToString();
+                    }
                 }
                 cont += 1;
-            }            
+            }                        
 
             // Guardamos el candidato en la base de datos
             string sql = $"INSERT INTO candidato VALUES ({candidato.Cedula}, '{candidato.Nombre}', '{candidato.Apellidos}', '{candidato.Fecha_nacimiento}', '{candidato.Fecha_aplicacion}')";
