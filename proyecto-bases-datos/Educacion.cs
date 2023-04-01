6adcd701db81d3f32e9105701a43a952af6baf0f
BrandonGC <brangmz20@gmail.com>
2023-04-01 13:15:55 -0600

    Creacion clases Educacion / Experiencia

---
+++ proyecto-bases-datos/Educacion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_bases_datos
+{
+    class Educacion
+    {
+        private string centro_educativo;
+        private string fecha_inicio;
+        private string fecha_finalizacion;
+        private string descripcion;
+
+        public Educacion()
+        {
+            this.centro_educativo = "";
+            this.fecha_inicio = "";
+            this.fecha_finalizacion = "";
+            this.descripcion = "";
+        }
+
+        public string Centro_educativo
+        {
+            get { return centro_educativo; }
+            set { centro_educativo = value; }
+        }
+
+        public string Fecha_inicio
+        {
+            get { return fecha_inicio; }
+            set { fecha_inicio = value; }
+        }
+
+        public string Fecha_finalizacion
+        {
+            get { return fecha_finalizacion; }
+            set { fecha_finalizacion = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+    }
+}
---
+++ proyecto-bases-datos/Experiencia.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proyecto_bases_datos
+{
+    class Experiencia
+    {
+        private string empresa;
+        private string fecha_inicio;
+        private string fecha_finalizacion;
+        private string descripcion;
+
+        public Experiencia()
+        {
+            this.empresa = "";
+            this.fecha_inicio = "";
+            this.fecha_finalizacion = "";
+            this.descripcion = "";
+        }
+
+        public string Empresa
+        {
+            get { return empresa; }
+            set { empresa = value; }
+        }
+
+        public string Fecha_inicio
+        {
+            get { return fecha_inicio; }
+            set { fecha_inicio = value; }
+        }
+
+        public string Fecha_finalizacion
+        {
+            get { return fecha_finalizacion; }
+            set { fecha_finalizacion = value; }
+        }
+
+        public string Descripcion
+        {
+            get { return descripcion; }
+            set { descripcion = value; }
+        }
+    }
+}
