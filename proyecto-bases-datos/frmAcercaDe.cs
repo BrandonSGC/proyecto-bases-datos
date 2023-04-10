a833c80d8248340a92407334d081bfe5fbaee169
Genesis-BQ <126612658+Genesis-BQ@users.noreply.github.com>
2023-04-10 17:46:45 -0600

    Crear Acerca De

---
+++ proyecto-bases-datos/frmAcercaDe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace proyecto_bases_datos
+{
+    public partial class frmAcercaDe : Form
+    {
+        public frmAcercaDe()
+        {
+            InitializeComponent();
+        }
+
+        private void pbAtras_Click(object sender, EventArgs e)
+        {
+            frmPrincipal frmPantalla = new frmPrincipal();
+            frmPantalla.Show();
+            this.Hide();
+        }
+    }
+}
